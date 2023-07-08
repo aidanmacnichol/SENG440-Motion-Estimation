d81c5703e18b7a6d11bf0e704cc0b5c5ce25c88b
amacnichol <amacnichol@corvusenergy.com>
2023-07-08 11:16:31 -0700

    attempting SAD implementation

--- loadImage.C
+++ loadImage.C
@@ -39,10 +39,41 @@ void printArray(loadImg* img) {
     }
 }
 
+
+/*
+320 / 16 = 20 blocks in the x direction 
+240 / 16 = 15 blocks in the y direciton 
+
+*/
+void SAD(int A, int B){
+    int A[16][16], B[16][16], diff, sad = 0; 
+    int i, j; 
+    for(i=0; i<16; i++){
+        for(j=0; j<16; j++){
+            diff = A[x+i][y+j] - B[(x+r) + i][(y+s) + j]; 
+            if(diff < 0){
+                diff -= diff;
+            } 
+        sad += diff; 
+        }
+    }
+}
+
 int main() {
-    // loadImg forward;
-    // loadImage(&forward, "forward.txt");
-    // printArray(&forward);
+    // Load reference frame
+    loadImg reference;
+    loadImage(&reference, "reference.txt")
+
+    for(int i = 0; i < 16)
+    int[16][16] =
+    SAD(&reference)
+
+    //Load forward frame 
+    loadImg forward;
+    loadImage(&forward, "forward.txt");
+
+
+    printArray(&forward);
 
     return 0;
 }
