03431477d4dbe80c1b32452a61782a7ffe2b9dbb
amacnichol <amacnichol@corvusenergy.com>
2023-07-20 14:42:14 -0700

    added SAD function

--- loadImage.C
+++ loadImage.C
@@ -41,12 +41,14 @@ void printArray(loadImg* img) {
 
 
 /*
-320 / 16 = 20 blocks in the x direction 
-240 / 16 = 15 blocks in the y direciton 
+Perform SAD on two 16x16 blocks, B is the reference block and A is the current comparison block
 
+(x,y) is the position of current block
+(r,s) is the motion vector (displacement of current block A relative to reference block B)
 */
-void SAD(int A, int B){
-    int A[16][16], B[16][16], diff, sad = 0; 
+
+int SAD(int A[16][16], int B[16][16], int x, int y, int r, int s, int bestMatch){
+    int diff, sad = 0; 
     int i, j; 
     for(i=0; i<16; i++){
         for(j=0; j<16; j++){
@@ -57,23 +59,63 @@ void SAD(int A, int B){
         sad += diff; 
         }
     }
+    //printf("SAD: %d\n", sad);
+    // Check if current SAD is better than the best match
+    if(sad < bestMatch){
+        bestMatch = sad;
+    }
+    return bestMatch; 
 }
 
 int main() {
     // Load reference frame
     loadImg reference;
-    loadImage(&reference, "reference.txt")
+    loadImage(&reference, "reference.txt");
 
-    for(int i = 0; i < 16)
-    int[16][16] =
-    SAD(&reference)
+    // Load reference block
+
+    // I want to load the first 16x16 block of the reference array into a new one
+    int blockB[16][16];
+
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++){
+            blockB[i][j] = reference.array[i][j];
+        }
+    }
 
     //Load forward frame 
     loadImg forward;
     loadImage(&forward, "forward.txt");
 
+    int blockA[16][16];
 
-    printArray(&forward);
+
+    /*
+    (320 - 16 = 304)
+    (240 - 16 = 224)
+    top left pixel is the reference pixel.
+    These x,y loops loop through all possible x,y positions of the forward frame (block A)
+    */
+
+    int bestMatch = 200; 
+
+    for(int x = 1; x < 304; x++){
+        for(int y = 1; y < 224; y++){
+
+            // Load 16x16 block A from forward image
+            for(int i = 0; i < 16; i++){
+                for(int j = 0; j < 16; j++){
+                    blockA[i][j] = forward.array[i][j];
+                }
+            }
+        // Perform SAD current block
+        // in this example with only one reference block the current position is the same as the motion vector
+        bestMatch = SAD(blockA, blockB, x, y, x, y, bestMatch);
+        
+        }
+    }
+
+    printf("Best Match: %d\n", bestMatch);
 
     return 0;
 }
